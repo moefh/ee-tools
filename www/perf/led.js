f57f3effcd79d8f72593499118e12933da892d36
Ricardo Massaro <ricardo.massaro@gmail.com>
2020-05-18 15:38:13 -0300

    Improve LED display

--- www/perf/led.js
+++ www/perf/led.js
@@ -67,41 +67,38 @@ class LEDConfig {
     // ===========================================================================
     // === draw ==================================================================
 
-    drawArc(ctx, mode, cx, cy, radius, start, end, color) {
-        //console.log(color, '->', '#' + color.map(n => ('00' + n.toString(16)).substr(-2)).join(''));
-        if (mode == 'fill') {
-            ctx.lineWidth = 4;
-            ctx.fillStyle = '#' + color.map(n => ('00' + n.toString(16)).substr(-2)).join('');
-        } else
-            ctx.strokeStyle = '#' + color.map(n => ('00' + n.toString(16)).substr(-2)).join('');
-        
+    fillArc(ctx, cx, cy, radius, start, end, color) {
+        ctx.fillStyle = '#' + color.map(n => ('00' + n.toString(16)).substr(-2)).join('');
         ctx.beginPath();
         ctx.arc(cx, cy, radius, start, end);
-        if (mode == 'fill')
-            ctx.fill();
-        else
-            ctx.stroke();
+        ctx.fill();
+    }
+
+    drawArc(ctx, cx, cy, radius, start, end, width, color) {
+        ctx.lineWidth = width;
+        ctx.strokeStyle = '#' + color.map(n => ('00' + n.toString(16)).substr(-2)).join('');
+        ctx.beginPath();
+        ctx.arc(cx, cy, radius, start, end);
+        ctx.stroke();
     }
 
     clamp(val, low, high) {
         return (val < low) ? low : (val > high) ? high : val;
     }
-    
+
     draw(ctx, el) {
         let radius = (el.info && el.info.size == 1) ? 10 : 18;
         let color = (el.info && this.colors[el.info.color]) ? this.colors[el.info.color] : this.colors.red;
 
-        //console.log('drawing with color', color, (el.info) ? el.info.color : 'default');
-        
-        let low_color = color.map((c) => Math.floor(c*0.8));
+        let low_color  = color.map((c) => Math.floor(c*0.8));
         let high_color = color.map((c) => this.clamp(c+192, 0, 255));
 
         let cx = el.sx*this.board.hole_size/2-this.board.hole_size/2;
         let cy = el.sy*this.board.hole_size/2-this.board.hole_size/2;
 
-        this.drawArc(ctx, 'fill',   cx, cy,     radius,  0.1*Math.PI, -0.1*Math.PI, low_color);
-        this.drawArc(ctx, 'fill',   cx, cy, 0.8*radius,  0.1*Math.PI, -0.1*Math.PI, color);
-        this.drawArc(ctx, 'stroke', cx, cy, 0.5*radius,  1.1*Math.PI - el.rot/2*Math.PI,  1.5*Math.PI - el.rot/2*Math.PI, high_color);
+        this.fillArc(ctx, cx, cy,     radius,  0.15*Math.PI, -0.15*Math.PI, low_color);
+        this.fillArc(ctx, cx, cy, 0.8*radius,  0,            2*Math.PI,     color);
+        this.drawArc(ctx, cx, cy, 0.5*radius,  1.1*Math.PI - el.rot/2*Math.PI,  1.5*Math.PI - el.rot/2*Math.PI, (radius==10) ? 2 : 4, high_color);
     }
         
 }
